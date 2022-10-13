079a77047363a46f32003cdbe3bb45cd7935cbef
dannyamuche <preciousamuche@gmail.com>
2022-10-13 14:02:46 -0700

    C Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program.
+ * Return - If no errors, non zero will be returned.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
