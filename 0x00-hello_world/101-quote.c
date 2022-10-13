93ffb9ff0c670c1e24beaf72cfe52c29b912bc34
dannyamuche <preciousamuche@gmail.com>
2022-10-13 15:01:33 -0700

    Dora Korpar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry
@@ -7,3 +8,4 @@ int main(void)
 {
 	write(1, "and that pieve of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
