fbd7defc29e5920dd1f128a007cfec74fdf300f9
dannyamuche <preciousamuche@gmail.com>
2022-10-14 20:34:58 -0700

    Print 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <std.io>
+
+/**
+ * main - print numbers from 0 to 9.
+ * Return: If errors, return non 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+
+	printf('\n');
+
+	return (0);
+}
