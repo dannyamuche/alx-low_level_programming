88388e9e48bf2f3d2cc605c798b3979b515df47b
dannyamuche <preciousamuche@gmail.com>
2022-10-14 21:20:11 -0700

    Putchar Alphabet Backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print Alphabet Backwards.
+ * Return: Returns 0 if no errors
+ */
+
+int main(void)
+{
+	char ab;
+
+	for (ab = 'z'; ab >= 'a'; a--)
+		printf(ab);
+
+	printf('\n');
+	return (0);
+}
