c988557bee125873e04a12b160dcb4eec25630e2
dannyamuche <preciousamuche@gmail.com>
2022-10-14 19:56:59 -0700

    Upper and Lower Putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower and upper cases
+ * Return: if error, return non zero
+ */
+
+int main(void)
+{
+	char ul;
+
+	for (ul = 'a'; ul <= 'z'; ul++)
+		putchar(ul);
+
+	for (ul = 'A'; ul <= 'Z'; ul++)
+		putchar(ul);
+
+	putchar('\n');
+
+	return (0);
