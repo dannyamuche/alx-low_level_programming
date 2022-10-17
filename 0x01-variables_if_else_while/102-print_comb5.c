3f16f2f87bc16b882d49243abb45488efa52e4a2
dannyamuche <preciousamuche@gmail.com>
2022-10-16 23:11:27 -0700

    Combo of 2 Twos

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,17 +8,17 @@ int main(void)
 {
 	int m, n;
 
-	for (m = '0'; m <= 99; m++)
+	for (m = 0; m < 99; m++)
 	{
 		for (n = m + 1; n <= 99; n++)
 		{
-			putchar((m / 10) + '0');
-			putchar((m % 10) + '0');
+			putchar(m / 10 + '0');
+			putchar(m % 10 + '0');
 
 			putchar(' ');
 
-			putchar((n / 10) + '0');
-			putchar((n % 10) + '0');
+			putchar(n / 10 + '0');
+			putchar(n % 10 + '0');
 
 			if (m == 98 && n == 99)
 			{
