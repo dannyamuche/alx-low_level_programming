6551dad9e88299cfaf6c31cc2d497cd175dcc11b
dannyamuche <preciousamuche@gmail.com>
2022-10-16 22:39:35 -0700

    Combo of 2 Twos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Print Combo of 2 Twos
+ * Return: Returns 0 if successful.
+ */
+
+int main(void)
+{
+	int m, n;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		for (n + 1; n <= '9'; n++)
+		{
+			putchar((10 / m) + '0');
+			putchar((10 % m) + '0');
+
+			putchar(' ');
+
+			putchar((10 / n) + '0');
+			putchar((10 % n) + '0');
+
+			if (m == 98 && n == 99)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
