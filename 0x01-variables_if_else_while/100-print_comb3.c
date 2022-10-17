d909a5f15b3769f4140881a787b7db156034891a
dannyamuche <preciousamuche@gmail.com>
2022-10-16 18:56:15 -0700

    Combo of Two

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - This is to print combo of twos
+ * Return: Returns 0 if successful
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			if (i != j)
+			{
+				putchar (i);
+				putchar (j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == 8 && j == 9)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
