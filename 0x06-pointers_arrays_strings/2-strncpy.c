136be580f7064a39fc224ac090e4215f09889834
dannyamuche <preciousamuche@gmail.com>
2022-10-26 18:31:50 -0700

    Strin Copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,11 +3,11 @@
 /**
  * *_strncpy - copies a string with n
  * @dest: copy to
- * @str: copy from
+ * @src: copy from
  * @n: number of char to be printed
  * Return: dest
  */
-char *_strncpy(char *dest, char *str, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
