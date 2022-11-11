db099a7834f014d0273a1c1dd6463ac8d2100274
dannyamuche <preciousamuche@gmail.com>
2022-11-11 15:26:57 -0800

    Allocates Memory for Array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * _calloc - allocated memoria for nmeb
+  * @nmemb: number of element in the array
+  * @size: bytes of each array
+  * Return: a pointer
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+		if (nmemb == 0 || size == 0)
+			return (NULL);
+
+	p = malloc(nmemb * size);
+		if (p == NULL)
+			return (NULL);
+
+		for (i = 0; i < nmemb * size; i++)
+			p[i] = 0;
+
+		return (p);
+}
